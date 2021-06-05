d759a2b6e397763fb9a9d924d5969f784d23a690
HanShuliangwt-maker <18392356893@163.com>
2021-06-05 08:08:56 +0800

    docs: 配置文件修改

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import "./wdyr";
 import ReactDOM from "react-dom";
 import { DevTools, loadServer } from "jira-dev-tool";
 import App from "./App";
@@ -7,12 +7,12 @@ import AppProviders from "context";
 
 loadServer(() =>
   ReactDOM.render(
-    <React.StrictMode>
-      <AppProviders>
-        <DevTools />
-        <App />
-      </AppProviders>
-    </React.StrictMode>,
+    /* <React.StrictMode> */
+    <AppProviders>
+      <DevTools />
+      <App />
+    </AppProviders>,
+    /* </React.StrictMode>, */
     document.getElementById("root")
   )
 );
